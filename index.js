1a76d7471b0090044783ddd4e7a0cdec077ba653
sethcarrino <sethcarrino@gmail.com>
2017-07-24 15:23:44 -0400

    finished project

---
+++ index.js
@@ -0,0 +1,33 @@
+const moment = require("moment");
+const chalk = require("chalk");
+
+const date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+const day = moment().format("DDDo");
+const seconds = Math.round((moment() - moment().startOf("day"))/1000)
+
+
+console.log("It is " + chalk.blue(date) + ".");
+
+console.log("It is the " + chalk.magenta(day) + " day of the year.");
+
+console.log("It is " + chalk.cyan(seconds) + " seconds into the day.");
+
+function dst() {
+  if (moment().isDST() === true) {
+    return "It " + chalk.green("is") + " during Daylight Savings Time.";
+  } else if (moment().isDST() === false){
+    return "It " + chalk.green("is not") + " during Daylight Savings Time.";
+  }
+}
+
+console.log(dst(moment().isDST()));
+
+function leap() {
+  if (moment().isLeapYear() === true) {
+    return "It " + chalk.red(is) + " not a leap year.";
+  } else if (moment().isLeapYear() === false){
+    return "It " + chalk.red("is not") + " not a leap year.";
+  }
+}
+
+console.log(leap());
